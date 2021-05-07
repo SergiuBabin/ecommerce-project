2c1c24bc829969ac00aa6cccd36e8246ff6a8820
Sergiu Babin <48562108+SergiuBabin@users.noreply.github.com>
2021-05-07 22:51:17 +0300

    v2.0

--- Backend/Spring-boot-ecommerce/src/main/java/md/SpringREST/ecommerce/dao/ProductRepository.java
+++ Backend/Spring-boot-ecommerce/src/main/java/md/SpringREST/ecommerce/dao/ProductRepository.java
@@ -2,10 +2,11 @@ package md.SpringREST.ecommerce.dao;
 
 import md.SpringREST.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
  * @author Babin Sergiu
  */
-
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
